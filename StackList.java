797b6920d17e259e05daf8d029f48ec29925dbbf
brandonmenv <brg_mev@hotmail.com>
2014-08-01 21:41:06 -0600

    El stack de la implementacion list
    NO SIRVE TODAVIA

---
+++ StackList.java
@@ -0,0 +1,54 @@
+/**
+   * @StackList.java  1.0 01/08/14
+   *
+   * Universidad del Valle de Guatemala.
+   * Seccion: 30 Estructura de Datos
+   * @author Brandon Mendez Carnet: 13087
+   * @author Ricardo Franco: 13261
+   * StackList: Stack para la implementacion de List
+   * 
+   *
+   */
+
+import java.util.ArrayList;
+
+public class StackList<E>
+ extends AbstractStack<E>
+{
+	protected ArrayList<E> data;
+
+	public StackList()
+	// post: constructs a new, empty stack
+	{
+		data = new ArrayList<E>();
+	}
+
+	public void push(E item)
+	// post: the value is added to the stack
+	//          will be popped next if no intervening push
+	{
+		data.add(item);
+	}
+
+	public E pop()
+	// pre: stack is not empty
+	// post: most recently pushed item is removed and returned
+	{
+		return data.remove(size()-1);
+	}
+
+	public E peek()
+	// pre: stack is not empty
+	// post: top value (next to be popped) is returned
+	{
+		return data.get(size() - 1);
+	}
+	
+	public int size()
+	// post: returns the number of elements in the stack
+	{
+		return data.size();
+	}
+  
+	
+}
